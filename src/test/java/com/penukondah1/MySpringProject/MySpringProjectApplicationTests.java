99cda544c46c7df137a3f5d3644488dbad0b0c0c
harika penukonda <penukondaharika@gmail.com>
2020-07-01 15:02:03 -0500

    Create MySpringProjectApplicationTests.java

---
+++ src/test/java/com/penukondah1/MySpringProject/MySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penukondah1.MySpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
