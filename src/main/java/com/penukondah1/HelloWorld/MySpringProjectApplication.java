63304befe7270e28b53a89508457748d5f67c4c0
harika penukonda <penukondaharika@gmail.com>
2020-07-02 10:58:17 -0500

    Create MySpringProjectApplication.java

---
+++ src/main/java/com/penukondah1/HelloWorld/MySpringProjectApplication.java
@@ -0,0 +1,15 @@
+package com.penukondah1.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // enables auto-configuration and also to indicate the application is Spring Boot
+public class MySpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringProjectApplication.class, args);
+		//MySpringProjectApplication class must be executed
+	}
+	
+
+}
