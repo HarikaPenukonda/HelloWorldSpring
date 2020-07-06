cac8912e0dffc154bb666dca9daf95f933761b80
harika penukonda <penukondaharika@gmail.com>
2020-07-06 18:25:14 -0500

    Update StudentService.java

--- src/main/java/com/penukondah1/HelloWorld/Service/StudentService.java
+++ src/main/java/com/penukondah1/HelloWorld/Service/StudentService.java
@@ -1,5 +1,6 @@
 package com.penukondah1.HelloWorld.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,13 +14,13 @@ public class StudentService {
 	
 	/* Using Arrays.asList method to create this list in line and calling the constructor on the student class
 	 * to create new student objects*/
-	private List<Student> students = Arrays.asList(
+	private List<Student> students = new ArrayList<>(Arrays.asList(
 			new Student("101","Archie","ECE"),
 			new Student("102","Betty","CSE"),
 			new Student("103","Cheryl","EEE"),
 			new Student("104","Juniper","MECH"),
 			new Student("105","Dagwood","CE")
-			);
+			));
 	
 	public List<Student> getAllStudents(){
 		return students;
@@ -32,5 +33,31 @@ public class StudentService {
 		 * findFirst to get the very first student element and get() to get student instance*/
 		
 	}
+	
+	public void addStudent(Student student)
+	{
+		students.add(student);
+	}
+
+	public void updateStudent(Student student, String id) 
+	{
+		for(int i=0;i<students.size();i++)
+		{
+			Student s = students.get(i);
+			if(s.getSid().equals(id))
+			{
+				students.set(i, student);
+			}
+		}
+		
+	}
+
+	public void deleteStudent(String id) 
+	{
+		students.removeIf(t -> t.getSid().equals(id));
+		
+	}
+
+	
 
 }
