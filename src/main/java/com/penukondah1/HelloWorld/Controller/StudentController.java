50d7653160aa4f1bf2996b5a5251f18763e491a9
harika penukonda <penukondaharika@gmail.com>
2020-07-06 14:51:20 -0500

    Update StudentController.java

--- src/main/java/com/penukondah1/HelloWorld/Controller/StudentController.java
+++ src/main/java/com/penukondah1/HelloWorld/Controller/StudentController.java
@@ -1,33 +1,42 @@
 package com.penukondah1.HelloWorld.Controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.penukondah1.HelloWorld.Model.Student;
+import com.penukondah1.HelloWorld.Service.StudentService;
 
 @RestController
 public class StudentController {
 	
-	@RequestMapping("/student")
+	//instance of studentservice - no need to create a new instance every time, Singleton needs the instance that spring created
+	//private member variable 
+	@Autowired //depencency to student service -- for depency injection 
+	private StudentService studentService;
+	
+	@RequestMapping("/students")
 	public List<Student> getAllStudents()
 	{
-		/* Using Arrays.asList method to create this list in line and calling the constructor on the student class
-		 * to create new student objects*/
-		return Arrays.asList(
-				new Student(101,"Archie","ECE"),
-				new Student(102,"Betty","CSE"),
-				new Student(103,"Cheryl","EEE"),
-				new Student(104,"Juniper","MECH"),
-				new Student(105,"Dagwood","CE"));
 		
+		return studentService.getAllStudents();
+	}	
 		/* Spring MVC - when there is /student request it maps getAllStudents method , execute that method and returns 
 		 * the object. The returned object is gonna get converted to JSON automatically and send back as HTTP response*/
 		
 		/*The generated JSON has key names corresponding to property names of the student class. The JSON values are 
 		 * values of those properties*/
+		
+	@RequestMapping("/students/{id}") //{id} to inform spring that it is variable {token}
+	public Student getStudent(@PathVariable String id) 
+	/*@PathVariable to send the variable {id} as the argument te that respective method
+	It maps {id} to String id of the method getStudent*/
+	{
+		return studentService.getStudent(id);
 	}
+	
 
 }
